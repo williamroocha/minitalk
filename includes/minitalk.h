75f11f153887399b9f6a6e224429d75ad01b24c6
William Ferreira Rocha <wiferrei@c2r10s1.42lisboa.com>
2023-09-18 17:21:13 +0100

    Start the project

---
+++ includes/minitalk.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wiferrei < wiferrei@student.42lisboa.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 15:47:20 by wiferrei          #+#    #+#             */
+/*   Updated: 2023/09/18 17:11:44 by wiferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+#
+# include "../ft_printf/includes/ft_printf.h"
+# include "../libft/includes/libft.h"
+# include <signal.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+#endif
