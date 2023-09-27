54d3ee5b61662e52461d80cacde2cba0e3ff6cd3
William Rocha <william@pop-os.localdomain>
2023-09-27 09:43:58 +0100

    wtf i am doing?

--- src/client/client.c
+++ src/client/client.c
@@ -6,83 +6,86 @@
 /*   By: wiferrei <wiferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 16:45:27 by wiferrei          #+#    #+#             */
-/*   Updated: 2023/09/22 09:18:18 by wiferrei         ###   ########.fr       */
+/*   Updated: 2023/09/27 09:31:46 by wiferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-void	check_error_args(int ac, char **av)
+void	args_check(int ac, char **av)
 {
 	int	i;
 
 	i = 0;
 	if (ac != 3)
-		ft_error_handler("Wrong number of arguments");
+		ft_putstr_fd("Error: Wrong number of arguments\n", 2);
 	while (av[1][i])
 	{
 		if (!ft_isdigit(av[1][i]))
-			ft_error_handler("Invalid PID");
+			ft_putstr_fd("Error: PID must be a number\n", 2);
 		i++;
 	}
-	if (av[2] == NULL)
-		ft_error_handler("Invalid message");
+	if (*av[2] == '\0')
+		ft_putstr_fd("Error: Empty string\n", 2);
 }
 
-void	send_message(pid_t sv_pid, char *msg)
+void	sig_handler(int signum)
+{
+	if (signum == SIGUSR1)
+		ft_putstr_fd("Message received by server\n", 1);
+	else if (signum == SIGUSR2)
+		ft_putstr_fd("Message received by server\n", 1);
+	else
+		ft_putstr_fd("Error: Unknown signal\n", 2);
+	exit(0);
+}
+
+void	config_sigaction(void)
+{
+	struct sigaction	sa_newsig;
+
+	sa_newsig.sa_handler = &sig_handler;
+	sa_newsig.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGUSR1, &sa_newsig, NULL) == -1)
+		ft_putstr_fd("Failed to change SIGUSR1's behavior", 2);
+	if (sigaction(SIGUSR2, &sa_newsig, NULL) == -1)
+		ft_putstr_fd("Failed to change SIGUSR2's behavior", 2);
+}
+
+void	send_message(pid_t server_pid, char *message)
 {
 	unsigned char	c;
 	int				nbr_bits;
 
-	while (*msg)
+	while (*message)
 	{
-		c = *msg;
+		c = *message;
 		nbr_bits = 8;
 		while (nbr_bits--)
 		{
 			if (c & 0b10000000)
-				kill(sv_pid, SIGUSR1);
+				kill(server_pid, SIGUSR1);
 			else
-				kill(sv_pid, SIGUSR2);
+				kill(server_pid, SIGUSR2);
 			usleep(50);
 			c <<= 1;
 		}
-		msg++;
+		message++;
 	}
-}
-
-void	handle_sigusr(int signum)
-{
-	if (signum == SIGUSR2)
-		ft_printf("Character has been successfully received!\n");
-}
-
-void	configure_sigaction(void)
-{
-	struct sigaction	sa;
-
-	sa.sa_handler = &handle_sigusr;
-	sa.sa_flags = SA_SIGINFO;
-	sigemptyset(&sa.sa_mask);
-	if (sigaction(SIGUSR1, &sa, NULL) == -1)
-		ft_error_handler("Failed to configure SIGUSR1\n");
-	if (sigaction(SIGUSR2, &sa, NULL) == -1)
-		ft_error_handler("Failed to configure SIGUSR2\n");
 }
 
 int	main(int ac, char **av)
 {
-	pid_t	sv_pid;
+	pid_t	server_pid;
 
-	check_error_args(ac, av);
-	sv_pid = ft_atoi(av[1]);
-	ft_printf("Client PID: %d\n", (int)getpid());
-	configure_sigaction();
-	ft_printf("Sending message: %s\n", av[2]);
-	send_message(sv_pid, av[2]);
+	args_check(ac, av);
+	server_pid = ft_atoi(av[1]);
+	ft_printf("Client PID: %d\n", getpid());
+	config_sigaction();
+	send_message(server_pid, av[2]);
 	while (1)
 	{
 		pause();
 	}
-	return (EXIT_SUCCESS);
+	return (0);
 }
--- src/server/server.c
+++ src/server/server.c
@@ -6,19 +6,20 @@
 /*   By: wiferrei <wiferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 22:15:31 by william           #+#    #+#             */
-/*   Updated: 2023/09/22 09:23:49 by wiferrei         ###   ########.fr       */
+/*   Updated: 2023/09/27 09:42:56 by wiferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-void	handle_sigusr(int signum, siginfo_t *info, void *ucontent)
+void	handle_sigusr(int signum, siginfo_t *info, void *ucontext)
 {
 	static int				bit_itr;
 	static unsigned char	c;
 
 	bit_itr = -1;
-	(void)ucontent;
+	c = 0;
+	(void)ucontext;
 	if (bit_itr < 0)
 		bit_itr = 7;
 	if (signum == SIGUSR1)
@@ -26,26 +27,28 @@ void	handle_sigusr(int signum, siginfo_t *info, void *ucontent)
 	bit_itr--;
 	if (bit_itr < 0 && c)
 	{
-		ft_putchar_fd(c, STDOUT_FILENO);
+		ft_putchar_fd(c, 1);
 		c = 0;
 		if (kill(info->si_pid, SIGUSR2) == -1)
-			ft_error_handler("Server failed to send SIGUSR2");
+			ft_putstr_fd("Failed to send confirmation signal (SIGUSR2)", 2);
+		fflush(stdout); // Flush output
 		return ;
 	}
 	if (kill(info->si_pid, SIGUSR1) == -1)
-		ft_error_handler("Failed to send SIGUSR1");
+		ft_putstr_fd("Failed to send confirmation signal (SIGUSR1)", 2);
+	fflush(stdout); // Flush output
 }
 
-void	configure_sigaction(void)
+void	config_sigaction(void)
 {
-	struct sigaction	sa_newsignal;
+	struct sigaction	sa_newsig;
 
-	sa_newsignal.sa_sigaction = &handle_sigusr;
-	sa_newsignal.sa_flags = SA_SIGINFO;
-	if (sigaction(SIGUSR1, &sa_newsignal, NULL) == -1)
-		ft_error_handler("Failed to configure SIGUSR1\n");
-	if (sigaction(SIGUSR2, &sa_newsignal, NULL) == -1)
-		ft_error_handler("Failed to configure SIGUSR2\n");
+	sa_newsig.sa_sigaction = &handle_sigusr;
+	sa_newsig.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGUSR1, &sa_newsig, NULL) == -1)
+		ft_putstr_fd("Failed to change SIGUSR1's behavior", 2);
+	if (sigaction(SIGUSR2, &sa_newsig, NULL) == -1)
+		ft_putstr_fd("Failed to change SIGUSR2's behavior", 2);
 }
 
 int	main(void)
@@ -53,11 +56,10 @@ int	main(void)
 	pid_t	server_pid;
 
 	server_pid = getpid();
-	ft_printf("Server PID: %d\n", (int)server_pid);
-	ft_printf("Waiting for signals...\n");
+	ft_printf("Server PID: %d\n", server_pid);
 	while (1)
 	{
-		configure_sigaction();
+		config_sigaction();
 	}
-	return (EXIT_SUCCESS);
+	return (0);
 }
