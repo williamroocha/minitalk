f1df5c43d487ad401441fef399c5ebeb61c5b48c
William Ferreira Rocha <wiferrei@c1r3s1.42lisboa.com>
2023-09-27 19:29:40 +0100

    Implemented the comunication logic, need to improve the code

--- src/client/client.c
+++ src/client/client.c
@@ -6,86 +6,51 @@
 /*   By: wiferrei <wiferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 16:45:27 by wiferrei          #+#    #+#             */
-/*   Updated: 2023/09/27 09:31:46 by wiferrei         ###   ########.fr       */
+/*   Updated: 2023/09/27 18:57:22 by wiferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-void	args_check(int ac, char **av)
+void	send_bit(char *txt, int pid_id)
 {
-	int	i;
+	int		i;
+	int		base;
+	char	ch;
 
-	i = 0;
-	if (ac != 3)
-		ft_putstr_fd("Error: Wrong number of arguments\n", 2);
-	while (av[1][i])
+	i = -1;
+	while (txt[++i])
 	{
-		if (!ft_isdigit(av[1][i]))
-			ft_putstr_fd("Error: PID must be a number\n", 2);
-		i++;
-	}
-	if (*av[2] == '\0')
-		ft_putstr_fd("Error: Empty string\n", 2);
-}
-
-void	sig_handler(int signum)
-{
-	if (signum == SIGUSR1)
-		ft_putstr_fd("Message received by server\n", 1);
-	else if (signum == SIGUSR2)
-		ft_putstr_fd("Message received by server\n", 1);
-	else
-		ft_putstr_fd("Error: Unknown signal\n", 2);
-	exit(0);
-}
-
-void	config_sigaction(void)
-{
-	struct sigaction	sa_newsig;
-
-	sa_newsig.sa_handler = &sig_handler;
-	sa_newsig.sa_flags = SA_SIGINFO;
-	if (sigaction(SIGUSR1, &sa_newsig, NULL) == -1)
-		ft_putstr_fd("Failed to change SIGUSR1's behavior", 2);
-	if (sigaction(SIGUSR2, &sa_newsig, NULL) == -1)
-		ft_putstr_fd("Failed to change SIGUSR2's behavior", 2);
-}
-
-void	send_message(pid_t server_pid, char *message)
-{
-	unsigned char	c;
-	int				nbr_bits;
-
-	while (*message)
-	{
-		c = *message;
-		nbr_bits = 8;
-		while (nbr_bits--)
+		base = 128;
+		ch = txt[i];
+		while (base > 0)
 		{
-			if (c & 0b10000000)
-				kill(server_pid, SIGUSR1);
+			if (ch >= base)
+			{
+				kill(pid_id, SIGUSR1);
+				ch -= base;
+			}
 			else
-				kill(server_pid, SIGUSR2);
-			usleep(50);
-			c <<= 1;
+				kill(pid_id, SIGUSR2);
+			base = base >> 1;
+			usleep(250);
 		}
-		message++;
 	}
 }
 
 int	main(int ac, char **av)
 {
-	pid_t	server_pid;
-
-	args_check(ac, av);
-	server_pid = ft_atoi(av[1]);
-	ft_printf("Client PID: %d\n", getpid());
-	config_sigaction();
-	send_message(server_pid, av[2]);
-	while (1)
+	if (ft_isdigit(av[1] == 0))
 	{
-		pause();
+		ft_putstr_fd("Error: PID must be a number\n", 2);
+		return (0);
+	}
+	else
+	{
+		if (ac == 3)
+			send_bit(av[2], ft_atoi(av[1]));
+		else
+			ft_printf("(Error) Try ./client <PID> <MSG>\n");
 	}
 	return (0);
 }
--- src/server/server.c
+++ src/server/server.c
@@ -6,60 +6,53 @@
 /*   By: wiferrei <wiferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 22:15:31 by william           #+#    #+#             */
-/*   Updated: 2023/09/27 09:42:56 by wiferrei         ###   ########.fr       */
+/*   Updated: 2023/09/27 18:57:04 by wiferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minitalk.h"
 
-void	handle_sigusr(int signum, siginfo_t *info, void *ucontext)
-{
-	static int				bit_itr;
-	static unsigned char	c;
+static int	g_bin[8] = {0};
 
-	bit_itr = -1;
-	c = 0;
-	(void)ucontext;
-	if (bit_itr < 0)
-		bit_itr = 7;
-	if (signum == SIGUSR1)
-		c |= (1 << bit_itr);
-	bit_itr--;
-	if (bit_itr < 0 && c)
+void	convert_to_txt(int *bin)
+{
+	int		i;
+	int		base;
+	int		convert;
+	char	c;
+
+	base = 1;
+	i = 7;
+	convert = 0;
+	while (i > 0)
 	{
-		ft_putchar_fd(c, 1);
-		c = 0;
-		if (kill(info->si_pid, SIGUSR2) == -1)
-			ft_putstr_fd("Failed to send confirmation signal (SIGUSR2)", 2);
-		fflush(stdout); // Flush output
-		return ;
+		convert += (base * bin[i--]);
+		base = base << 1;
 	}
-	if (kill(info->si_pid, SIGUSR1) == -1)
-		ft_putstr_fd("Failed to send confirmation signal (SIGUSR1)", 2);
-	fflush(stdout); // Flush output
+	c = (char)convert;
+	ft_putchar_fd(c, 1);
 }
 
-void	config_sigaction(void)
+void	save_bin(int bit)
 {
-	struct sigaction	sa_newsig;
+	static int	i;
 
-	sa_newsig.sa_sigaction = &handle_sigusr;
-	sa_newsig.sa_flags = SA_SIGINFO;
-	if (sigaction(SIGUSR1, &sa_newsig, NULL) == -1)
-		ft_putstr_fd("Failed to change SIGUSR1's behavior", 2);
-	if (sigaction(SIGUSR2, &sa_newsig, NULL) == -1)
-		ft_putstr_fd("Failed to change SIGUSR2's behavior", 2);
+	if (bit == SIGUSR1)
+		g_bin[i++] = 1;
+	else if (bit == SIGUSR2)
+		g_bin[i++] = 0;
+	if (i == 8)
+	{
+		convert_to_txt(g_bin);
+		i = 0;
+	}
 }
 
 int	main(void)
 {
-	pid_t	server_pid;
-
-	server_pid = getpid();
-	ft_printf("Server PID: %d\n", server_pid);
+	ft_printf("Server PID: %d\n", getpid());
+	signal(SIGUSR1, save_bin);
+	signal(SIGUSR2, save_bin);
 	while (1)
-	{
-		config_sigaction();
-	}
-	return (0);
+		pause();
 }
